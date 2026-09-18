78817fc0ae7fd85700b3a90bfbbfb2e1f14c326d
zhongsanming <sanming.zhong@compiler-dev.com>
2026-09-18 10:44:18 +0800

    update flir commit id

--- python/setup_tools/utils/__init__.py
+++ python/setup_tools/utils/__init__.py
@@ -10,8 +10,8 @@ flagtree_submodules = {
                  commit_id="5842469a16b261e45a2c67fbfc308057622b03ee",
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "triton_shared")),
     "flir":
-    tools.Module(name="flir", url="https://github.com/flagos-ai/flir.git",
-                 commit_id="516ad3110cbf6112bbed8f927ae606c627a193f2",
+    tools.Module(name="flir", url="https://github.com/kateyijian/flir.git",
+                 commit_id="d8d3c6a64b5ed929618f90e4e09e0451712fb915",
                  dst_path=os.path.join(flagtree_configs.flagtree_submodule_dir, "flir")),
     "flagprism":
     tools.Module(name="FlagPrism", url="https://github.com/flagos-ai/FlagPrism.git",
